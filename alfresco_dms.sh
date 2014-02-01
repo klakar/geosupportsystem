e827d054cea092ac1b2c9cef2897c47820c5bd17
Klas Karlsson <klaskarlsson@hotmail.com>
2014-02-01 16:14:25 +0100

    Update alfresco_dms.sh

--- alfresco_dms.sh
+++ alfresco_dms.sh
@@ -6,3 +6,18 @@ IPA=$(ifconfig $1 | grep "inet addr" | awk -F: '{print $2}' | grep "Bcast" | awk
 echo "*****************************************************"
 echo " http://geosupportsystem.wordpress.com"
 echo "*****************************************************"
+echo " Detta installerar Alfresco Dokumenthantering "
+echo "*****************************************************"
+echo " Anteckna IP: $IPA"
+echo "*****************************************************"
+echo " Tryck valfri tangent för att starta..."
+read d
+
+# Uppdatera systemet
+sudo apt-get update
+sudo apt-get -y upgrade
+
+
+# Hämta installationsfilen
+wget http://eu.dl.alfresco.com/release/community/build-4848/alfresco-community-4.2.e-installer-linux-x64.bin
+sudo alfresco-community-4.2.e-installer-linux-x64.bin --mode text
