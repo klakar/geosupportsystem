57e65d528e8488720700705e0163936027380000
Klas Karlsson <klaskarlsson@hotmail.com>
2014-02-01 17:04:00 +0100

    Update alfresco_dms.sh

--- alfresco_dms.sh
+++ alfresco_dms.sh
@@ -8,8 +8,6 @@ echo " http://geosupportsystem.wordpress.com"
 echo "*****************************************************"
 echo " Detta installerar Alfresco Dokumenthantering "
 echo "*****************************************************"
-echo " Anteckna IP: $IPA"
-echo "*****************************************************"
 echo " Tryck valfri tangent för att starta..."
 read d
 
@@ -17,7 +15,17 @@ read d
 sudo apt-get update
 sudo apt-get -y upgrade
 
+# Hämtar och installerar stödfiler
+sudo apt-get -y install libice6 libsm6 libxt6 libxrender1 libfontconfig1 libcups2
 
 # Hämta installationsfilen
 wget http://eu.dl.alfresco.com/release/community/build-4848/alfresco-community-4.2.e-installer-linux-x64.bin
-sudo alfresco-community-4.2.e-installer-linux-x64.bin --mode text
+sudo chmod 777 alfresco-community-4.2.e-installer-linux-x64.bin
+sudo ./alfresco-community-4.2.e-installer-linux-x64.bin --mode text
+
+# Färdig
+clear
+echo "*****************************************************"
+echo " Logga in med angivna uppggifter på:"
+echo " http://$IPA:8080/share/"
+echo "*****************************************************"
