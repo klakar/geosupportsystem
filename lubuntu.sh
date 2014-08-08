6b7a0e167d45e27a2f2744f78255953010212eb1
Klas Karlsson <klakar@users.noreply.github.com>
2014-08-08 17:55:02 +0200

    Update lubuntu.sh

--- lubuntu.sh
+++ lubuntu.sh
@@ -29,6 +29,8 @@ apt-get install -y qgis python-qgis qgis-plugin-grass
 cd ~
 wget http://qgis.boundlessgeo.com/opengeo/version/0.5/download/
 unzip index.html
+wget https://github.com/alexbruy/photo2shape/archive/master.zip
+unzip master.zip
 apt-get install -y postgresql-9.3-postgis-2.1
 passwd postgres
 echo "Type: \password postgres"
