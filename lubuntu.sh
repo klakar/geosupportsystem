1b5f4e4a056e5f759bc3d2140535872467d28082
Klas Karlsson <klakar@users.noreply.github.com>
2014-08-12 21:12:54 +0200

    Update lubuntu.sh

--- lubuntu.sh
+++ lubuntu.sh
@@ -30,10 +30,10 @@ cd ~
 wget http://qgis.boundlessgeo.com/opengeo/version/0.5/download/
 unzip index.html
 mv opengeo/ /usr/share/qgis/python/plugins/opengeo/
-apt-get install build-essential python-all-dev git vim python-dev python-pip python-software-properties g++ gcc make libssl-dev libreadline6-dev libaio-dev libbz2-dev zlib1g-dev libjpeg62-dev libpcre3-dev libexpat1-dev libxml2 libxml2-dev libjson0 libjson0-dev liblzma-dev libevent-dev wget zip unzip
-apt-get install postgresql-9.3 postgresql-client-9.3 postgresql-contrib-9.3 libpq-dev postgresql-server-dev-9.3
-apt-get install binutils libproj-dev gdal-bin libgeoip1 libgtk2.0
-apt-get install postgresql-9.3-postgis-2.1
+apt-get install -y build-essential python-all-dev git vim python-dev python-pip python-software-properties g++ gcc make libssl-dev libreadline6-dev libaio-dev libbz2-dev zlib1g-dev libjpeg62-dev libpcre3-dev libexpat1-dev libxml2 libxml2-dev libjson0 libjson0-dev liblzma-dev libevent-dev wget zip unzip
+apt-get install -y postgresql-9.3 postgresql-client-9.3 postgresql-contrib-9.3 libpq-dev postgresql-server-dev-9.3
+apt-get install -y binutils libproj-dev gdal-bin libgeoip1 libgtk2.0
+apt-get install -y postgresql-9.3-postgis-2.1
 apt-get install -y pgadmin3
 mkdir /etc/skel/Desktop
 cd /etc/skel/Desktop/
