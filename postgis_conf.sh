a9114d8efd659aae8b0b40d316787b5581aa7659
Klas Karlsson <klakar@users.noreply.github.com>
2014-08-12 20:51:04 +0200

    Update postgis_conf.sh

--- postgis_conf.sh
+++ postgis_conf.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-DATUM=$(date)
-IPA=$(ifconfig $1 | grep "inet addr" | awk -F: '{print $2}' | grep "Bcast" | awk '{print $1}')
-clear
 apt-get install apt-get install build-essential python-all-dev git vim python-dev python-pip python-software-properties g++ gcc make libssl-dev libreadline6-dev libaio-dev libbz2-dev zlib1g-dev libjpeg62-dev libpcre3-dev libexpat1-dev libxml2 libxml2-dev libjson0 libjson0-dev liblzma-dev libevent-dev wget zip unzip
 apt-get install postgresql-9.3 postgresql-client-9.3 postgresql-contrib-9.3 libpq-dev postgresql-server-dev-9.3
 apt-get install binutils libproj-dev gdal-bin libgeoip1 libgtk2.0
