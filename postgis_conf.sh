6b4cf7e754108eacb30b964d66773df198f01ffd
Klas Karlsson <klakar@users.noreply.github.com>
2014-08-12 20:38:56 +0200

    Update postgis_conf.sh

--- postgis_conf.sh
+++ postgis_conf.sh
@@ -2,31 +2,8 @@
 DATUM=$(date)
 IPA=$(ifconfig $1 | grep "inet addr" | awk -F: '{print $2}' | grep "Bcast" | awk '{print $1}')
 clear
-echo "Set password for user postgres"
-sudo passwd postgres
-clear
-echo "Set password for database user postgres:"
-echo "1. Type command: \password postgres"
-echo "2. Set (and repeat) password"
-echo "3. Exit/continue with: \q"
-sudo -s -u postgres psql
-clear
-UT="#listen_addresses = 'localhost'"
-IN="listen_addresses = '$IPA,localhost'"
-sudo sed -i "s/$UT/$IN/g" /etc/postgresql/9.3/main/postgresql.conf
-RYMD=$(echo $IPA | awk -F. '{print $1"."$2"."$3".1/24"}')
-ny_rad="host all all $RYMD md5"
-sudo sed -i "/IPv4/a $ny_rad" /etc/postgresql/9.3/main/pg_hba.conf
-sudo /etc/init.d/postgresql restart
-echo "Create database called 'geodata'"
-sudo -u postgres createdb geodata
-sudo -u postgres psql -d geodata -f /usr/share/postgresql/9.3/contrib/postgis-2.1/postgis.sql
-sudo -u postgres psql -d geodata -f /usr/share/postgresql/9.3/contrib/postgis-2.1/spatial_ref_sys.sql
-sudo -u postgres psql -d geodata -f /usr/share/postgresql/9.3/contrib/postgis-2.1/postgis_comments.sql
-sudo -u postgres psql -d geodata -f /usr/share/postgresql/9.3/contrib/postgis-2.1/rtpostgis.sql
-sudo -u postgres psql -d geodata -f /usr/share/postgresql/9.3/contrib/postgis-2.1/raster_comments.sql
-sudo -u postgres psql -d geodata -f /usr/share/postgresql/9.3/contrib/postgis-2.1/topology.sql
-sudo -u postgres psql -d geodata -f /usr/share/postgresql/9.3/contrib/postgis-2.1/topology_comments.sql
+apt-get install apt-get install build-essential python-all-dev git vim python-dev python-pip python-software-properties g++ gcc make libssl-dev libreadline6-dev libaio-dev libbz2-dev zlib1g-dev libjpeg62-dev libpcre3-dev libexpat1-dev libxml2 libxml2-dev libjson0 libjson0-dev liblzma-dev libevent-dev wget zip unzip
+
 clear
 echo "pgAdminIII (user/pass): postgres/*******"
 echo
