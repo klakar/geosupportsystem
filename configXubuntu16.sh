8ded2d9dd47268cc1ba85b3c28a228fe43c3f325
Klas Karlsson <klakar@users.noreply.github.com>
2016-06-12 12:15:45 +0200

    Update configXubuntu16.sh

--- configXubuntu16.sh
+++ configXubuntu16.sh
@@ -6,7 +6,7 @@ wget http://downloadcontent.opensuse.org/repositories/home:/Horst3180/xUbuntu_15
 sudo dpkg -i arc-theme-solid_1465131682.3095952_all.deb
 wget http://ppa.launchpad.net/tiheum/equinox/ubuntu/pool/main/f/faenza-icon-theme/faenza-icon-theme_1.3.1_all.deb
 sudo dpkg -i faenza-icon-theme_1.3.1_all.deb
-sudo apt-get install libdvd-pkg
+sudo apt-get install -y libdvd-pkg
 sudo dpkg-reconfigure libdvd-pkg
 sudo apt-get dist-upgrade
 sudo apt-get install -y software-properties-common
@@ -15,12 +15,12 @@ echo deb http://ppa.launchpad.net/kdenlive/kdenlive-stable/ubuntu xenial main | 
 sudo apt-get -y install leafpad vlc browser-plugin-vlc kodi kdenlive darktable steam wine playonlinux gimp inkscape docky gufw python-gi
 wget https://download.skype.com/linux/skype-ubuntu-precise_4.3.0.37-1_i386.deb
 sudo dpkg -i skype-ubuntu-precise_4.3.0.37-1_i386.deb
-sudo apt-get install -f
+sudo apt-get -y install -f
 wget https://linux.dropbox.com/packages/ubuntu/dropbox_2015.10.28_amd64.deb
 sudo dpkg -i dropbox_2015.10.28_amd64.deb
 keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt-get update && sudo apt-get install spotify-client
+sudo apt-get update && sudo apt-get -y install spotify-client
 echo deb http://qgis.org/debian xenial main | sudo tee /etc/apt/sources.list.d/qgis.list
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key 3FF5FFCAD71472C4
 sudo apt-get update && sudo apt-get -y install qgis python-qgis qgis-plugin-grass
@@ -32,7 +32,7 @@ echo 4. Add "super" KEYBOARD shortcut
 echo 5. Change Theme in APPEARANCE
 echo 6. Change Icons in APPEARANCE
 echo 7. Change the DESKTOP wallpaper and icons
-echo 8. WINDOW MANAGER TWEAKS compositor
+echo 8. WINDOW MANAGER style and TWEAKS compositor
 echo 9. Start DOCKY and add launchers
 echo 10. Tweak LIGHTDM greeter
 echo
