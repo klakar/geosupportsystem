93d9388d8b9ee96cd92587df8fa0b9cda943a7e0
Klas Karlsson <klakar@users.noreply.github.com>
2016-06-17 18:23:50 +0200

    More software added to the install list
    meshlab, freecad, pgadmin3 gpsbabel blender hugin handbreak audacity synfigstudio virtualbox mixxx...

--- configXubuntu16.sh
+++ configXubuntu16.sh
@@ -11,8 +11,13 @@ sudo dpkg-reconfigure libdvd-pkg
 sudo apt-get dist-upgrade
 sudo apt-get install -y software-properties-common
 sudo add-apt-repository ppa:team-xbmc/ppa
+sudo add-apt-repository ppa:hugin/hugin-builds
+sudo add-apt-repository ppa:mixxx/mixxx
 echo deb http://ppa.launchpad.net/kdenlive/kdenlive-stable/ubuntu xenial main | sudo tee /etc/apt/sources.list.d/kdenlive.list
+sudo apt-get update
 sudo apt-get -y install leafpad vlc browser-plugin-vlc kodi kdenlive darktable steam wine playonlinux gimp inkscape docky gufw python-gi
+sudo apt-get -y install meshlab freecad pgadmin3 gpsbabel gpsbabel-gui mtkbabel lauchy blender hugin enblend panini handbrake
+sudo apt-get -y install audacity synfigstudio virtualbox mixxx
 wget https://download.skype.com/linux/skype-ubuntu-precise_4.3.0.37-1_i386.deb
 sudo dpkg -i skype-ubuntu-precise_4.3.0.37-1_i386.deb
 sudo apt-get -y install -f
@@ -20,10 +25,9 @@ wget https://linux.dropbox.com/packages/ubuntu/dropbox_2015.10.28_amd64.deb
 sudo dpkg -i dropbox_2015.10.28_amd64.deb
 keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt-get update && sudo apt-get -y install spotify-client
 echo deb http://qgis.org/debian xenial main | sudo tee /etc/apt/sources.list.d/qgis.list
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key 3FF5FFCAD71472C4
-sudo apt-get update && sudo apt-get -y install qgis python-qgis qgis-plugin-grass
+sudo apt-get update && sudo apt-get -y install qgis python-qgis qgis-plugin-grass spotify-client
 clear
 echo 1. Change the WHISKER MENU Properties
 echo 2. Check for ADDITIONAL DRIVERS
@@ -35,6 +39,7 @@ echo 7. Change the DESKTOP wallpaper and icons
 echo 8. WINDOW MANAGER style and TWEAKS compositor
 echo 9. Start DOCKY and add launchers
 echo 10. Tweak LIGHTDM greeter
+echo 11. Add Launchy to AUTOSTART
 echo
 echo Reboot - Log In - Reboot
 echo
