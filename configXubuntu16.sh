4cc1a786a824f6a3cb62b9e33b5b137e70a9a3ae
Klas Karlsson <klakar@users.noreply.github.com>
2016-06-19 19:44:30 +0200

    Update configXubuntu16.sh

--- configXubuntu16.sh
+++ configXubuntu16.sh
@@ -1,6 +1,6 @@
 sudo apt-get update && sudo apt-get -y upgrade
 sudo apt-get install -y xubuntu-restricted-extras libavcodec-extra
-sudo apt install -y tlp tlp-rdw
+sudo apt-get install -y tlp tlp-rdw
 sudo tlp start
 wget http://downloadcontent.opensuse.org/repositories/home:/Horst3180/xUbuntu_15.10/all/arc-theme-solid_1465131682.3095952_all.deb
 sudo dpkg -i arc-theme-solid_1465131682.3095952_all.deb
@@ -10,17 +10,23 @@ sudo apt-get install -y libdvd-pkg
 sudo dpkg-reconfigure libdvd-pkg
 sudo apt-get dist-upgrade
 sudo apt-get install -y software-properties-common
-sudo add-apt-repository ppa:team-xbmc/ppa
-sudo add-apt-repository ppa:hugin/hugin-builds
-sudo add-apt-repository ppa:mixxx/mixxx
-sudo add-apt-repository ppa:moka/daily
-sudo add-apt-repository ppa:numix/ppa
-sudo add-apt-repository ppa:alecive/antigone
+sudo add-apt-repository -y ppa:team-xbmc/ppa
+sudo add-apt-repository -y ppa:hugin/hugin-builds
+sudo add-apt-repository -y ppa:mixxx/mixxx
+sudo add-apt-repository -y ppa:moka/daily
+sudo add-apt-repository -y ppa:numix/ppa
+sudo add-apt-repository -y ppa:alecive/antigone
 echo deb http://ppa.launchpad.net/kdenlive/kdenlive-stable/ubuntu xenial main | sudo tee /etc/apt/sources.list.d/kdenlive.list
 sudo apt-get update
-sudo apt-get install leafpad vlc browser-plugin-vlc kodi kdenlive darktable steam wine playonlinux gimp inkscape docky gufw python-gi
-sudo apt-get install meshlab freecad pgadmin3 gpsbabel gpsbabel-gui mtkbabel lauchy blender hugin enblend panini handbrake indicator-cpufreq
-sudo apt-get install audacity synfigstudio virtualbox mixxx moka-icon-theme numix-icon-theme numix-icon-theme-circle awoken-icon-theme
+sudo apt-get install -y leafpad vlc browser-plugin-vlc kodi kdenlive
+sudo apt-get install -y darktable steam wine playonlinux gimp inkscape
+sudo apt-get install -y docky gufw python-gi
+sudo apt-get update
+sudo apt-get install -y meshlab freecad pgadmin3 gpsbabel gpsbabel-gui mtkbabel
+sudo apt-get install -y lauchy blender hugin enblend panini handbrake indicator-cpufreq
+sudo apt-get update
+sudo apt-get install -y moka-icon-theme numix-icon-theme numix-icon-theme-circle awoken-icon-theme
+sudo apt-get install -y audacity synfigstudio virtualbox mixxx 
 wget https://download.skype.com/linux/skype-ubuntu-precise_4.3.0.37-1_i386.deb
 wget https://raw.githubusercontent.com/klakar/geosupportsystem/master/Systems-Linux-icon.png
 sudo mv Systems-Linux-icon.png /usr/share/icons/default/Systems-Linux-icon.png
@@ -32,7 +38,7 @@ keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
 echo deb http://qgis.org/debian xenial main | sudo tee /etc/apt/sources.list.d/qgis.list
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key 3FF5FFCAD71472C4
-sudo apt-get update && sudo apt-get -y install qgis python-qgis qgis-plugin-grass spotify-client
+sudo apt-get update && sudo apt-get install -y qgis python-qgis qgis-plugin-grass spotify-client
 clear
 echo 1. Change the WHISKER MENU Properties
 echo 2. Check for ADDITIONAL DRIVERS
