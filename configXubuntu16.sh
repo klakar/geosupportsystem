ae10610bd5f1deabb8821d82c64dd0b719bc2900
Klas Karlsson <klakar@users.noreply.github.com>
2016-06-26 18:38:35 +0200

    Update configXubuntu16.sh

--- configXubuntu16.sh
+++ configXubuntu16.sh
@@ -39,9 +39,10 @@ sudo add-apt-repository -y -u ppa:team-xbmc/ppa
 sudo add-apt-repository -y -u ppa:hugin/hugin-builds
 sudo add-apt-repository -y -u ppa:mixxx/mixxx
 sudo add-apt-repository -y -u ppa:kdenlive/kdenlive-stable
+sudo add-apt-repository -y -u ppa:obsproject/obs-studio
 clear
 echo Installing videoplayers, audio software and editors...
-sudo apt-get install -y -qq leafpad vlc browser-plugin-vlc kodi kdenlive audacity mixxx
+sudo apt-get install -y -qq leafpad vlc browser-plugin-vlc kodi kdenlive audacity mixxx obs-studio
 clear
 echo Installing photo and graphics software...
 sudo apt-get install -y -qq darktable  gimp inkscape blender hugin enblend handbrake synfigstudio
