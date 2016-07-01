440b2479df20ab895b2bcc106907f3e363b8d5fe
Klas Karlsson <klakar@users.noreply.github.com>
2016-07-01 19:39:14 +0200

    Update configXubuntu16.sh

--- configXubuntu16.sh
+++ configXubuntu16.sh
@@ -73,6 +73,7 @@ if [ "$scis" != "n" ]; then
   sudo add-apt-repository -y -u ppa:ubuntugis/ubuntugis-unstable
   sudo apt-get update -qq && sudo apt-get install -y -qq qgis python-qgis qgis-plugin-grass
   sudo apt-get install -y -qq saga python-saga libotb otb-bin python-otb python-exif
+  sudo apt-get install -y -qq pyqt4-dev-tools python-sphinx
 fi
 clear
 echo Installing some system software and utilities
