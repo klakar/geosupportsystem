83e7f05b6a09cfc43d61cda63ad2ac933417c441
Klas Karlsson <klakar@users.noreply.github.com>
2022-10-15 15:59:09 +0200

    Create geonode_docker.sh

---
+++ geonode_docker.sh
@@ -0,0 +1,30 @@
+sudo add-apt-repository ppa:ubuntugis/ppa
+sudo apt update -y
+
+sudo apt install -y python3-gdal=3.3.2+dfsg-2~focal2 gdal-bin=3.3.2+dfsg-2~focal2 libgdal-dev=3.3.2+dfsg-2~focal2
+sudo apt install -y python3-pip python3-dev python3-virtualenv python3-venv virtualenvwrapper
+sudo apt install -y libxml2 libxml2-dev gettext
+sudo apt install -y libxslt1-dev libjpeg-dev libpng-dev libpq-dev
+sudo apt install -y software-properties-common build-essential
+sudo apt install -y git unzip gcc zlib1g-dev libgeos-dev libproj-dev
+sudo apt install -y sqlite3 spatialite-bin libsqlite3-mod-spatialite
+sudo add-apt-repository universe
+sudo apt-get update -y
+sudo apt-get install -y git-core git-buildpackage debhelper devscripts
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+sudo apt-get update -y
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
+sudo apt autoremove --purge
+
+sudo usermod -aG docker ${USER}
+su ${USER}
+git clone https://github.com/GeoNode/geonode-project.git -b 4.x
+
+# Ubuntu
+source /usr/share/virtualenvwrapper/virtualenvwrapper.sh
+mkvirtualenv --python=/usr/bin/python3 my_geonode
